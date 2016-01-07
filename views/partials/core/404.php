b0131ceab5847fbdae6ffd817423dd65e0c900a8
Dustin Rea <drea08@gmail.com>
2016-01-07 09:26:27 +0000

    Added 404.php

---
+++ views/partials/core/404.php
@@ -0,0 +1,7 @@
+<section class="row">
+    <h1 class="col-xs-offset-1 col-xs-10">OOPS - 404</h1>
+    <p class="col-xs-offset-1 col-xs-10">
+        Something went wrong.  You are welcome to try again, or return to the
+        <a href="/">Home Page</a>.
+    </p>
+</section>
